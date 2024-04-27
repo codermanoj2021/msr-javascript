46677f5891569ca94794e7df1d92f5e3d29e6d47
codermanoj2021 <153446610+codermanoj2021@users.noreply.github.com>
2024-04-27 08:24:38 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// // node 01_basics/03_conversionOperation
+// let score="33abc";
+// score=null;
+// console.log(score);
+
+// console.log(typeof score);
+
+// let valueInNumber=Number(score);//if score is not a number then this function will convert into a number
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);//NaN = Not a Number
+
+// // agar javascript jabarjasti number mein convert karna chahe , toh wahpe NaN aa jata hai
+
+// // similarly we have other function like Number
+// let IsLoggedIn=null;
+// isLoggedIn=""
+
+// console.log(IsLoggedIn);
+
+// console.log(typeof IsLoggedIn);
+
+// let Boole=Boolean(IsLoggedIn);
+
+// console.log(Boole);
+
+// console.log(typeof Boole);
+
+let someNumber=33;
+
+console.log(typeof someNumber);
+
+let stringNumber=String(someNumber);
+
+console.log(typeof stringNumber);
