7a49d4d338606347b3efe4ed6b4710c6cb0ee7c4
codermanoj2021 <153446610+codermanoj2021@users.noreply.github.com>
2024-04-27 03:36:24 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+// node 01_basics/01_variables.js
+const accountId=198837;
+let accountEmail="manoj@gmail.com";
+var accountPassword="12344";
+
+accountCity="ghaziabad";
+
+let accountState;
+
+// accountId=19273; //not allowed
+
+accountEmail="msr@gamil.com";
+console.log(accountEmail);
+
+console.table([accountEmail,accountId,accountPassword,accountState,accountCity]);
+
+/*
+prefer not to use var 
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all javascript as newer version
+
+
+// alert("hello world"); // we are using node js not browser
+
+console.log(3+3);
+
+let name="manoj singh rawat";//string
+let age=18;//int
+let isLoggedIn=false;//bool
+
+// we also have BigInt
+// null is standalone value
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isLoggedIn);
+console.log(typeof null);
+console.log(typeof undefined);
